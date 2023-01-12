fdf020390f8e382c0f2d0fc92dc1b96b5f0c4a3f
Reitumetse <joycreamoeletsi@gmail.com>
2023-01-13 01:03:20 +0200

    minops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+calculates min operations
+"""
+from sys import setrecursionlimit
+
+
+def minOperations(n):
+
+    setrecursionlimit(10**6)
+
+    count = 0
+    inc = 2  # 3 4 5 6 etc.
+
+    # factor = 2
+    # no even division (prime) inc = n
+
+    if type(n) is not int or n < 2:
+        return 0
+
+    while n != 1:
+        if n % inc == 0:
+            count += inc  # 1
+            n /= inc
+            inc = 1  # resets for next loop
+        inc = inc + 1
+
+    return count
